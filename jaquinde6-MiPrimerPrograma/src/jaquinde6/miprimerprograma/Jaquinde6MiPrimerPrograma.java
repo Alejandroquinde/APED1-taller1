dc4f1a20724b7f15d82ff7a542762c1f9da932c6
Jhon Alejandro <jaquinde6@utpl.edu.ec>
2023-10-19 17:15:18 -0500

    miprimer programa

---
+++ jaquinde6-MiPrimerPrograma/src/jaquinde6/miprimerprograma/Jaquinde6MiPrimerPrograma.java
@@ -0,0 +1,10 @@
+
+package jaquinde6.miprimerprograma;
+
+public class Jaquinde6MiPrimerPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
